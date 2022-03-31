68f0c3abee998f0bf6bad85986b6476f2fa926cb
eminaxoxo <100778616+eminaxoxo@users.noreply.github.com>
2022-03-31 14:34:47 +0200

    first

---
+++ ex04/src/index.js
@@ -0,0 +1,12 @@
+function(main){
+    //only change code belowe this line
+    // only change code above this line
+
+return {
+    myDecimal, 
+    myDecimalE
+};
+}
+
+console.log(main());
+export default main;
