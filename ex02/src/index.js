cbceadd5ca6da119fd40c4c04979ab71229fed56
eminaxoxo <100778616+eminaxoxo@users.noreply.github.com>
2022-03-31 16:16:42 +0200

    Final

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,8 +1,11 @@
-function main(){
+function main() {
     var myRes = 75;
+    // only change code below this line
     myRes++;
+    // only change code above this line
+  
     return myRes;
-
-}
-console.log(main());
-module.exports = main;
+  }
+  
+  console.log(main());
+  module.exports = main;
--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,9 +1,11 @@
-function main(){
+function main() {
     var myRes = 16;
+    // only change code below this line
     myRes--;
+    // only change code above this line
+  
     return myRes;
-
-}
-console.log(main());
-
-module.exports = main;
+  }
+  
+  console.log(main());
+  module.exports = main;
